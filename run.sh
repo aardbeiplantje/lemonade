aebe29de8ae0b568929f115af7f0d8cddf3c37dc
Tim Aerts <aardbeiplantje@gmail.com>
2026-06-15 23:41:11 +0200

    add run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+HF_HOME=${HF_HOME:-lemonade-huggingface}
+DOCKER_IMAGE=${DOCKER_IMAGE:-local/ai/lemonade-gfx1151:latest}
+extra_args=""
+if [ ! -z "$LEMONADE_MODELS_PRESETS" ]; then
+    extra_args="$extra_args -v $LEMONADE_MODELS_PRESETS:/lemonade/llamacpp_presets.ini:ro"
+fi
+if [ ! -z "$LEMONADE_MODELS_DIR" ]; then
+    extra_args="$extra_args -v $LEMONADE_MODELS_DIR:/models:rw"
+fi
+docker stop lemonade >/dev/null 2>&1 || true
+docker rm   lemonade >/dev/null 2>&1 || true
+exec docker run --rm \
+    --detach \
+    --name lemonade \
+    --network=host \
+    -v $HF_HOME:/hf:rw \
+    $extra_args \
+    --device=/dev/kfd \
+    --device=/dev/dri \
+    --device=/dev/accel \
+    --group-add=video \
+    --ipc=host \
+    --shm-size=128GB \
+    --cap-add=SYS_PTRACE \
+    --security-opt seccomp=unconfined \
+    --group-add=109 \
+    --group-add=992 \
+    --group-add=1000 \
+    -v lemonade-flm-models:/lemonade-server//.config/flm/models \
+    ${DOCKER_IMAGE} \
+        $*
+
